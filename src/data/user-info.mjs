799336135274486074ce18aba7232994c2e6d436
Pamod Malith Theekshana <dev.pamodmalith@gmail.com>
2025-08-26 17:33:04 +0530

    add some dummy data files

---
+++ src/data/user-info.mjs
@@ -0,0 +1,22 @@
+export const usersInfo = [
+  {
+    id: 1,
+    name: "Pamod",
+    email: "pamod@example.com",
+  },
+  {
+    id: 2,
+    name: "Malith",
+    email: "malith@example.com",
+  },
+  {
+    id: 3,
+    name: "Kavindu",
+    email: "kavindu@example.com",
+  },
+  {
+    id: 4,
+    name: "John",
+    email: "john@example.com",
+  },
+];
