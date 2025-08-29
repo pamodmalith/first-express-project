61d674b84377dfdcded3eca206b85291642a69e2
Pamod Malith Theekshana <dev.pamodmalith@gmail.com>
2025-08-29 14:52:55 +0530

    Added test route

--- src/router/index.mjs
+++ src/router/index.mjs
@@ -1,10 +1,12 @@
 import { Router } from "express";
 import userRouter from "./user.mjs";
 import productRouter from "./product.mjs";
+import testRouter from "./test.mjs";
 
 const rootRouter = Router();
 
 rootRouter.use("/user", userRouter);
 rootRouter.use("/product", productRouter);
+rootRouter.use("/test", testRouter);
 
 export default rootRouter;
