75893794f983d513cb339cabde42620029e8f232
Pamod Malith Theekshana <dev.pamodmalith@gmail.com>
2025-08-31 13:12:00 +0530

    Add cookies

--- server.mjs
+++ server.mjs
@@ -1,9 +1,11 @@
 import express from "express";
 import rootRouter from "./src/router/index.mjs";
+import cookieParser from "cookie-parser";
 
 const server = express();
 
 server.use(express.json());
+server.use(cookieParser("mykey"));
 server.use("/api/v2", rootRouter);
 
 server.listen(4000, () => console.log("Server is running"));
--- src/router/test.mjs
+++ src/router/test.mjs
@@ -4,11 +4,32 @@ import { validatorMethod } from "../utils/validatorMethod.mjs";
 
 const testRouter = Router();
 
-testRouter.get("/abc", validatorMethod, (req, res) => {
-  const r = validationResult(req);
-  console.log(r.array());
-  if (r.array().length) return res.status(400).json({ errors: r.array() });
+// testRouter.get("/abc", validatorMethod, (req, res) => {
+//   const r = validationResult(req);
+//   console.log(r.array());
+//   if (r.array().length) return res.status(400).json({ errors: r.array() });
 
+//   res.sendStatus(200);
+// });
+
+testRouter.get("/get-cookie", (req, res) => {
+  res.cookie("pamodCookie", "cookieValue", {
+    maxAge: 1000 * 30,
+    httpOnly: true,
+    signed: true,
+  });
+  res.cookie("pamodCookie2", "cookieValue2", {
+    maxAge: 1000 * 60,
+    httpOnly: true,
+    signed: true,
+  });
+  res.sendStatus(200);
+});
+
+testRouter.get("/read-cookie", (req, res) => {
+  console.log(req.signedCookies.pamodCookie);
+  console.log(req.signedCookies["pamodCookie"]);
+  console.log(req.headers.cookie);
   res.sendStatus(200);
 });
 
